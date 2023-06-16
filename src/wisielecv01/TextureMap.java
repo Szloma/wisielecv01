d8f6d4fa96e1ebee7248063d07a837ddb392e376
dsds <s28002@pjwstk.edu.pl>
2023-06-17 00:07:22 +0200

    finally added proper textures

--- src/wisielecv01/TextureMap.java
+++ src/wisielecv01/TextureMap.java
@@ -8,12 +8,12 @@ public class TextureMap {
 	public static Map<Integer, String> createTextureMap() {
 		   Map<Integer, String> texturePaths = new HashMap<>();
 		   	texturePaths.put(7, "textures/blank.png");
-	        texturePaths.put(6, "textures/texture6.png");
-	        texturePaths.put(5, "textures/texture5.png");
-	        texturePaths.put(4, "textures/texture4.png");
-	        texturePaths.put(3, "textures/texture3.png");
-	        texturePaths.put(2, "textures/texture2.png");
-	        texturePaths.put(1, "textures/texture1.png");
+	        texturePaths.put(1, "textures/texture6.png");
+	        texturePaths.put(2, "textures/texture5.png");
+	        texturePaths.put(3, "textures/texture4.png");
+	        texturePaths.put(4, "textures/texture3.png");
+	        texturePaths.put(5, "textures/texture2.png");
+	        texturePaths.put(6, "textures/texture1.png");
 	        return texturePaths;
     }
 }
