61aa000997ab3c3d9ea469fd70903ef728a05cdb
dsds <s28002@pjwstk.edu.pl>
2023-06-17 00:37:07 +0200

    fixed the ability to add words

--- src/wisielecv01/WordBase.java
+++ src/wisielecv01/WordBase.java
@@ -1,71 +1,101 @@
 package wisielecv01;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.*;
 
 public class WordBase {
-    private Map<String, List<String>> dictionary;
+    private List<String> englishWords;
+    private List<String> polishWords;
+    private String language;
 
     public WordBase() {
-        dictionary = new HashMap<>();
-        initializedictionary();
-    }
-
-    private void initializedictionary() {
-        List<String> englishWords = Arrays.asList("george", "karen", "pjaots", "motorcycle", "bus");
-        List<String> polishWords = Arrays.asList("janusz", "grazyna", "pjatk", "motur", "autobus");
-
-        dictionary.put("english", englishWords);
-        dictionary.put("polish", polishWords);
-    }
-    public boolean addWord(String language, String word) {
-    	if (dictionary.containsKey(language)) {
-            List<String> wordList = dictionary.get(language);
-            if (!wordList.contains(word)) {
-                wordList.add(word);
-                return true;
-            }
-        } else {
-            return false;
-        }
-        return false;
+        this.englishWords = new ArrayList<>();
+        this.polishWords = new ArrayList<>();
+        this.language = "english";
+        initializeEnglishWords();
+        initializePolishWords();
     }
 
     public void setLanguage(String language) {
-        if (!dictionary.containsKey(language)) {
+        this.language = language;
+    }
+
+    public String getRandomWord() {
+        List<String> words = getWordsList();
+        Random random = new Random();
+        int index = random.nextInt(words.size());
+        return words.get(index);
+    }
+
+    public String getRandomWord(int length) {
+        List<String> words = getWordsList();
+        List<String> filteredWords = new ArrayList<>();
+
+        for (String word : words) {
+            if (word.length() == length) {
+                filteredWords.add(word);
+            }
+        }
+
+        Random random = new Random();
+        int index = random.nextInt(filteredWords.size());
+        return filteredWords.get(index);
+    }
+
+    public boolean addWord(String language,String word) {
+        List<String> words = getWordsList(language);
+
+        if (!words.contains(word.toLowerCase())) {
+            words.add(word.toLowerCase());
+            return true;
+        }
+
+        return false;
+    }
+
+    private List<String> getWordsList(String language) {
+        if (language.equals("english")) {
+            return englishWords;
+        } else if (language.equals("polish")) {
+            return polishWords;
+        } else {
             throw new IllegalArgumentException("Unsupported language: " + language);
         }
     }
 
-    public String getRandomWord() {
-        Random random = new Random();
-        List<String> words = new ArrayList<>(dictionary.values().stream().flatMap(List::stream).toList());
-        return words.get(random.nextInt(words.size()));
-    }
 
-    public String getRandomWord(int length) {
-        Random random = new Random();
-        List<String> words = new ArrayList<>(dictionary.values().stream().flatMap(List::stream).toList());
-        List<String> wordsWithLength = new ArrayList<>();
-
-        for (String word : words) {
-            if (word.length() == length) {
-                wordsWithLength.add(word);
-            }
-        }
-
-        if (wordsWithLength.isEmpty()) {
-            throw new IllegalArgumentException("No words found with specified length: " + length);
-        }
-
-        return wordsWithLength.get(random.nextInt(wordsWithLength.size()));
-    }
     public void DEBUGprintAllWords() {
-        for (Map.Entry<String, List<String>> entry : dictionary.entrySet()) {
-            String language = entry.getKey();
-            List<String> words = entry.getValue();
-            System.out.println("Language: " + language);
-            System.out.println("Words: " + words);
-            System.out.println();
+        List<String> words = getWordsList();
+        for (String word : words) {
+            System.out.println(word);
         }
+    }
+
+    private List<String> getWordsList() {
+        if (language.equals("english")) {
+            return englishWords;
+        } else if (language.equals("polish")) {
+            return polishWords;
+        } else {
+            throw new IllegalStateException("Unsupported language: " + language);
+        }
+    }
+
+    private void initializeEnglishWords() {
+        // Add English words to the English dictionary
+        englishWords.add("george");
+        englishWords.add("pjatk");
+        englishWords.add("cream");
+        englishWords.add("quaternioin");
+        // ...
+    }
+
+    private void initializePolishWords() {
+        // Add Polish words to the Polish dictionary
+        polishWords.add("janusz");
+        polishWords.add("pjatk");
+        polishWords.add("masz");
+        polishWords.add("twarz");
+        // ...
     }
 }
